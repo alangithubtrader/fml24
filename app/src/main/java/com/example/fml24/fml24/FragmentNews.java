3878c0eb1e22650d547eb0ba8b7d10523c0b0dcc
Alan Lau <alan.lau@trader.ca>
2016-07-15 23:12:45 -0700

    News - added comments to FragmentNews.java

--- app/src/main/java/com/example/fml24/fml24/FragmentNews.java
+++ app/src/main/java/com/example/fml24/fml24/FragmentNews.java
@@ -3,7 +3,6 @@ package com.example.fml24.fml24;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v4.app.ListFragment;
-import android.util.Log;
 
 import com.example.fml24.fml24.API.BaseApi;
 
@@ -11,13 +10,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 
 
@@ -26,26 +18,25 @@ import java.util.ArrayList;
  */
 public class FragmentNews extends ListFragment{
 
-    private ArrayList<News> news;
     private NewsAdaptor newsAdaptor;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        //So that we can run network on UI thread
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-
         StrictMode.setThreadPolicy(policy);
 
+        //Get all news annoucement from Api
         JSONArray jsonArrayOfNews = BaseApi.getHttpRequest("https://free-lottery.herokuapp.com/api/get_announcement.php");
 
+        //Parse all news and add it to an array list
         ArrayList<News> news = AddNewsToArrayList(jsonArrayOfNews);
-
         newsAdaptor = new NewsAdaptor(getActivity(), news);
 
+        //set all news into UI
         setListAdapter(newsAdaptor);
-
-
     }
 
     private ArrayList<News> AddNewsToArrayList(JSONArray jsonArray) {
