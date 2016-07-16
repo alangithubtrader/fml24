b10fbff5dd81df2a9a80fa17f9c2eafeb0ebc023
Alan Lau <alan.lau@trader.ca>
2016-07-15 21:15:27 -0700

    News - messy - but got new announcment to display on one of the news row

--- app/src/main/java/com/example/fml24/fml24/FragmentNews.java
+++ app/src/main/java/com/example/fml24/fml24/FragmentNews.java
@@ -1,19 +1,16 @@
 package com.example.fml24.fml24;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
+import android.os.StrictMode;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
+
 
 /**
  * Created by adu on 16-07-12.
@@ -27,8 +24,34 @@ public class FragmentNews extends ListFragment{
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+
+        String test = "";
+        try {
+            URL url = new URL("https://free-lottery.herokuapp.com/api/get_announcement.php");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                test = stringBuilder.toString();
+            }
+            finally{
+                urlConnection.disconnect();
+            }
+        }
+        catch(Exception e) {
+            Log.e("ERROR", e.getMessage(), e);
+        }
+
         news = new ArrayList<>();
-        news.add(new News("News Title 1", "Feb 6, 2010, 2:30pm", "Body1"));
+        news.add(new News("News Title 1", "Feb 6, 2010, 2:30pm", test));
         news.add(new News("News Title 2", "Feb 29, 2011, 7:30pm", "Body2"));
         news.add(new News("News Title 3", "Feb 19, 2016, 9:30pm", "Body3"));
 
@@ -38,5 +61,4 @@ public class FragmentNews extends ListFragment{
 
 
     }
-
 }
