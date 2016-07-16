b9de0a706f3878ec973b769b436084e3fb8c90a7
Alan Lau <alan.lau@trader.ca>
2016-07-15 22:10:35 -0700

    News - figured out how to get value from JSONARRAY

--- app/src/main/java/com/example/fml24/fml24/API/BaseApi.java
+++ app/src/main/java/com/example/fml24/fml24/API/BaseApi.java
@@ -3,7 +3,17 @@ package com.example.fml24.fml24.API;
 import android.os.StrictMode;
 import android.util.Log;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -13,7 +23,7 @@ import java.net.URL;
  */
 public class BaseApi {
 
-    public static String getHttpRequest(String urlInput) {
+    public static JSONArray getHttpRequest(String urlInput) {
 
         try {
             URL url = new URL(urlInput);
@@ -26,7 +36,9 @@ public class BaseApi {
                     stringBuilder.append(line).append("\n");
                 }
                 bufferedReader.close();
-                return stringBuilder.toString();
+                JSONTokener tokener = new JSONTokener(stringBuilder.toString());
+                JSONArray finalResult = new JSONArray(tokener);
+                return finalResult;
             }
             finally{
                 urlConnection.disconnect();
@@ -34,6 +46,20 @@ public class BaseApi {
         }
         catch(Exception e) {
             Log.e("ERROR", e.getMessage(), e);
+        }
+        return null;
+    }
+
+    public static String getHttpRequest2(String urlInput) throws IOException {
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpGet httpget= new HttpGet(urlInput);
+
+        HttpResponse response = httpclient.execute(httpget);
+
+        if(response.getStatusLine().getStatusCode()==200){
+             return EntityUtils.toString(response.getEntity());
+        } else {
+            Log.i("Server response", "Failed to get server response" );
         }
         return "";
     }
--- app/src/main/java/com/example/fml24/fml24/FragmentNews.java
+++ app/src/main/java/com/example/fml24/fml24/FragmentNews.java
@@ -7,7 +7,13 @@ import android.util.Log;
 
 import com.example.fml24.fml24.API.BaseApi;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -30,10 +36,21 @@ public class FragmentNews extends ListFragment{
 
         StrictMode.setThreadPolicy(policy);
 
-        String test = BaseApi.getHttpRequest("https://free-lottery.herokuapp.com/api/get_announcement.php");
-
+        JSONArray test = null;
+            test = BaseApi.getHttpRequest("https://free-lottery.herokuapp.com/api/get_announcement.php");
         news = new ArrayList<>();
-        news.add(new News("News Title 1", "Feb 6, 2010, 2:30pm", test));
+        try {
+
+            JSONObject jsonobject = test.getJSONObject(0);
+            String name = jsonobject.getString("description");
+
+
+            news.add(new News("News Title 1", "Feb 6, 2010, 2:30pm", name));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
         news.add(new News("News Title 2", "Feb 29, 2011, 7:30pm", "Body2"));
         news.add(new News("News Title 3", "Feb 19, 2016, 9:30pm", "Body3"));
 
