9f45fe16597aa1747caea18de8c190e491519edc
Alan Lau <alan.lau@trader.ca>
2016-07-17 22:15:53 -0700

    post user api logic integrated into LoginActivity.java

--- app/src/main/java/com/example/fml24/fml24/LoginActivity.java
+++ app/src/main/java/com/example/fml24/fml24/LoginActivity.java
@@ -29,9 +29,19 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
@@ -39,6 +49,10 @@ import static android.Manifest.permission.READ_CONTACTS;
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_EMAIL = "email";
+
 
     /**
      * Id to identity READ_CONTACTS permission request.
@@ -313,6 +327,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         private final String mEmail;
         private final String mPassword;
 
+        public static final String REGISTER_URL = "https://free-lottery.herokuapp.com/api/post_user.php";
+
+
         UserLoginTask(String email, String password) {
             mEmail = email;
             mPassword = password;
@@ -320,26 +337,50 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
             try {
                 // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
+                // TODO: attempt authentication against a network service.
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, REGISTER_URL,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
 
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
+                                if(response.matches(".*\\d+.*")) //does response contains any numbers?
+                                {
+                                    Toast.makeText(LoginActivity.this,"Login success.",Toast.LENGTH_LONG).show();
+                                    // TODO: some how we need to capture this user id for retrieving user info after successfully logged in.
+                                    return;
+
+                                }
+                                Toast.makeText(LoginActivity.this,"Register success.",Toast.LENGTH_LONG).show();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(LoginActivity.this,error.toString(), Toast.LENGTH_LONG).show();
+                            }
+                        }){
+                    @Override
+                    protected Map<String,String> getParams(){
+                    Map<String,String> params = new HashMap<String, String>();
+                    params.put(KEY_PASSWORD,mPassword);
+                    params.put(KEY_EMAIL, mEmail);
+                    return params;
+                }};
+
+                RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+                requestQueue.add(stringRequest);
+
+            } catch (Exception e) {
+                return false;
             }
 
             // TODO: register the new account here.
             return true;
         }
+
+
 
         @Override
         protected void onPostExecute(final Boolean success) {
