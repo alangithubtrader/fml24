2aa3f118bf8949460f70d3f2d75fe0e74635fb6f
Alan Lau <alan.lau@trader.ca>
2016-08-01 11:37:07 -0700

    More - added click listeners to each text view

--- app/src/main/java/com/example/fml24/fml24/MoreActivity.java
+++ app/src/main/java/com/example/fml24/fml24/MoreActivity.java
@@ -1,20 +1,27 @@
 package com.example.fml24.fml24;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by adu on 16-07-12.
  */
-public class MoreActivity extends AppCompatActivity{
+public class MoreActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView changeEmailAddressTextView;
+    private TextView reportAProblemTextView;
+    private TextView faqTextView;
+    private TextView gameRulesTextView;
+    private TextView followUsOnFacebookTextView;
+    private TextView termsAndConditionTextView;
 
     public MoreActivity()
     {
@@ -24,6 +31,54 @@ public class MoreActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_more);
+        setContentView(R.layout.activity_more);
+
+        changeEmailAddressTextView = (TextView) findViewById(R.id.change_email_address);
+        reportAProblemTextView = (TextView) findViewById(R.id.report_a_problem);
+        termsAndConditionTextView = (TextView) findViewById(R.id.terms_and_conditions);
+        faqTextView = (TextView) findViewById(R.id.faq);
+        gameRulesTextView = (TextView) findViewById(R.id.game_rules);
+        followUsOnFacebookTextView = (TextView)findViewById(R.id.follow_us_on_facebook);
+
+        changeEmailAddressTextView.setOnClickListener(this);
+        reportAProblemTextView.setOnClickListener(this);
+        faqTextView.setOnClickListener(this);
+        termsAndConditionTextView.setOnClickListener(this);
+        gameRulesTextView.setOnClickListener(this);
+        followUsOnFacebookTextView.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.change_email_address:
+                Toast.makeText(getApplicationContext(), "Change email address.", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.report_a_problem:
+                Toast.makeText(getApplicationContext(), "Report a problem.", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.terms_and_conditions:
+                Toast.makeText(getApplicationContext(), "Terms and condition.", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.faq:
+                Toast.makeText(getApplicationContext(), "FAQ.", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.game_rules:
+                Toast.makeText(getApplicationContext(), "Game Rules.", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.follow_us_on_facebook:
+                Toast.makeText(getApplicationContext(), "Follow us on facebook.", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                break;
+        }
     }
 }
